b5e9958a4b12ae59ef274957a55745fbe2e270bb
AkeroroH <156268545+akeroroh@users.noreply.github.com>
2025-05-30 15:18:04 +0900

    🔨 Feat | 탭뷰 세팅 완료

---
+++ F1T5/F1T5/Views/FavView/FavoriteView.swift
@@ -0,0 +1,22 @@
+//
+//  FavoriteView.swift
+//  F1T5
+//
+//  Created by 진아현 on 5/30/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct FavoriteView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("즐겨찾기 뷰입니다.")
+        }
+        .padding()
+        
+    }
+}
