7f228078a04d4d7711b356461c475d655f2d5982
AkeroroH <156268545+akeroroh@users.noreply.github.com>
2025-06-02 16:46:02 +0900

    🔨 Feat | 추천페이지 구성 중

--- F1T5/F1T5/Views/RecoView/RecommendView.swift
+++ F1T5/F1T5/Views/RecoView/RecommendView.swift
@@ -9,11 +9,53 @@ import Foundation
 import SwiftUI
 
 struct RecommendView: View {
+    static let gradientStart = Color.black
+    static let gradientEnd = Color.clear
+
+    
     var body: some View {
-        VStack {
-            Text("추천 페이지 뷰입니다.")
+        ZStack {
+            Image("recobackground")
+                .resizable()
+                .frame(height: 900)
+            
+            VStack {
+                ZStack {
+                    LinearGradient(
+                        gradient: Gradient(stops: [
+                            .init(color: Color.black.opacity(0.0), location: 0.0),
+                            .init(color: Color.black, location: 0.5)
+                        ]),
+                        startPoint: .bottom,
+                        endPoint: .top
+                    )
+                    .frame(height: 230)
+                    
+                    VStack {
+                        Text("앱이름의")
+                            .foregroundColor(Color.white)
+                            .font(.headline)
+                        Text("성준님을 위한 추천 식당이에요!")
+                            .foregroundColor(Color.white)
+                    }
+                }
+                .ignoresSafeArea()
+                
+                Spacer()
+                
+                ZStack {
+                    Rectangle()
+                        .fill(LinearGradient(
+                              gradient: .init(colors: [Self.gradientStart, Self.gradientEnd]),
+                              startPoint: .init(x: 0.5, y: 0.9),
+                              endPoint: .init(x: 0.5, y: 0)
+                            ))
+                        .frame(height: 150)
+                }
+            }
         }
-        .padding()
+        .ignoresSafeArea()
+
     }
 }
 
