c1f191d696423cb3e42c7ecc699b9fae881b9624
AkeroroH <156268545+akeroroh@users.noreply.github.com>
2025-06-03 20:56:48 +0900

    🔨 Feat | 아이템 뷰 전환 성공, 좋아요 연결

--- F1T5/F1T5/Views/RecoView/RecommendView.swift
+++ F1T5/F1T5/Views/RecoView/RecommendView.swift
@@ -8,14 +8,42 @@
 import Foundation
 import SwiftUI
 import SwiftData
+import SwiftUIPager
 
 struct RecommendView: View {
     @Query var restaurants: [Restaurant]
+    var recommendations: [Restaurant] {
+        recommendBasedOnUserRatings(from: restaurants)
+    }
+    
+    @State private var sharedImage: UIImage? = nil
+    
+    @State private var page: Page = .first()
     
     static let gradientStart = Color.black
     static let gradientEnd = Color.clear
     
     var body: some View {
+        GeometryReader { geometry in
+            Pager(page: page, data: recommendations, id: \.id) { restaurant in
+                //                sharedImage = recommendItem(restaurant).snapshot()
+                recommendItem(restaurant)
+                    .frame(
+                        width: geometry.size.width,
+                        height: geometry.size.height
+                    )
+            }
+            .vertical()
+            .pagingPriority(.simultaneous)
+            .interactive(scale: 0.95)
+            .itemSpacing(10)
+            .padding(.vertical, 10)
+        }
+        .ignoresSafeArea()
+    }
+    
+    @ViewBuilder
+    func recommendItem(_ restaurant: Restaurant) -> some View {
         ZStack {
             Image("recobackground")
                 .resizable()
@@ -41,7 +69,7 @@ struct RecommendView: View {
                     
                     VStack(alignment: .leading, spacing: 5) {
                         HStack {
-                            Text("앱이름")
+                            Text("포슐랭")
                                 .font(
                                     Font.custom("Apple SD Gothic Neo", size: 32)
                                         .weight(.bold)
@@ -60,7 +88,7 @@ struct RecommendView: View {
                         Text("성준님을 위한 추천 식당이에요!")
                             .font(
                                 Font.custom("Apple SD Gothic Neo", size: 22)
-                                .weight(.bold)
+                                    .weight(.bold)
                             )
                             .foregroundColor(Color.white)
                     }
@@ -72,44 +100,42 @@ struct RecommendView: View {
                 
                 ZStack {
                     Rectangle()
-                      .foregroundColor(.clear)
-                      .frame(width: 420, height: 421)
-                      .background(
-                        LinearGradient(
-                          stops: [
-                            Gradient.Stop(color: Color(red: 0.16, green: 0.16, blue: 0.2).opacity(0), location: 0.00),
-                            Gradient.Stop(color: Color(red: 0.16, green: 0.16, blue: 0.2), location: 0.57),
-                          ],
-                          startPoint: UnitPoint(x: 0.5, y: 0.1),
-                          endPoint: UnitPoint(x: 0.5, y: 1)
+                        .foregroundColor(.clear)
+                        .frame(width: 420, height: 421)
+                        .background(
+                            LinearGradient(
+                                stops: [
+                                    Gradient.Stop(color: Color(red: 0.16, green: 0.16, blue: 0.2).opacity(0), location: 0.00),
+                                    Gradient.Stop(color: Color(red: 0.16, green: 0.16, blue: 0.2), location: 0.57),
+                                ],
+                                startPoint: UnitPoint(x: 0.5, y: 0.1),
+                                endPoint: UnitPoint(x: 0.5, y: 1)
+                            )
                         )
-                      )
                     
                     HStack (alignment: .bottom){
                         VStack(alignment: .leading, spacing: 3) {
                             
-                            Text("\(restaurants.first?.area ?? "nil") • \(restaurants.first?.category.rawValue ?? "nil")")
+                            Text("\(restaurant.area) • \(restaurant.category.rawValue)")
                                 .font(
                                     Font.custom("Apple SD Gothic Neo", size: 17)
-                                    .weight(.bold)
+                                        .weight(.bold)
                                 )
                                 .foregroundColor(.white)
                                 .padding(.top, 50)
-                            Text(restaurants.first?.name ?? "nil")
+                            Text(restaurant.name)
                                 .font(
                                     Font.custom("Apple SD Gothic Neo", size: 28)
-                                    .weight(.heavy)
+                                        .weight(.heavy)
                                 )
                                 .foregroundColor(Color(red: 1, green: 0.58, blue: 0))
-                            Text(restaurants.first?.restaurantDescription ?? "nil")
+                            Text(restaurant.restaurantDescription)
                                 .font(Font.custom("Apple SD Gothic Neo", size: 17))
                                 .foregroundColor(.white)
                                 .lineLimit(1)
-                            Text("영업시간 \( restaurants.first?.weekdayHours ?? "nil")")
+                            Text("영업시간 \(restaurant.weekdayHours)")
                                 .font(Font.custom("Apple SD Gothic Neo", size: 13))
                                 .foregroundColor(.white)
-                            
-                            
                             
                             Button {
                                 
@@ -121,39 +147,39 @@ struct RecommendView: View {
                             
                             
                         }
-                        
-
+                        .padding(30)
                         
                         Spacer()
                         
                         VStack {
                             Button {
-                                
+                                restaurant.isFavorite = !restaurant.isFavorite
                             } label: {
-                                Text(Image(systemName: "heart"))
-                                    .foregroundColor(.white)
+                                if restaurant.isFavorite {
+                                    Text(Image(systemName: "heart.fill"))
+                                        .foregroundColor(Color.postechOrange)
+                                } else {
+                                    Text(Image(systemName: "heart"))
+                                        .foregroundColor(.white)
+                                }
                             }
                             .buttonStyle(CustomMainButtonStyle())
                             
-                            Button {
-                                
-                            } label: {
+                            ShareLink(item: URL(string: "https://developer.apple.com/xcode/swiftui/")!) {
                                 Text(Image(systemName: "arrowshape.turn.up.right.fill"))
                                     .foregroundColor(.white)
                             }
                             .buttonStyle(CustomMainButtonStyle())
-
                         }
-                        
-                    
+                        .padding(30)
                     }
-                    .padding(30)
+                    .padding(.bottom, 70)
                 }
-                
             }
+            .ignoresSafeArea()
+            
         }
-        .ignoresSafeArea()
-
+        
     }
 }
 
@@ -185,6 +211,7 @@ struct CustomMainButtonStyle: ButtonStyle {
         }
     }
 }
+
 
 
 #Preview {
