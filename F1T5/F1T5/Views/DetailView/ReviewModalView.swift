51a7cfc12187aa0c8d7656fb550d49bee0e2360c
HanHyeBeen <vvv5974033@gmail.com>
2025-06-04 14:47:20 +0900

    🔨 Feat | DetailView _ Review Modal 라벨 수정 기능 개발
    -라벨 선택 후 리뷰쓰기 버튼 클릭 시 해당 라벨로 수정되는 기능 추가

--- F1T5/F1T5/Views/DetailView/ReviewModalView.swift
+++ F1T5/F1T5/Views/DetailView/ReviewModalView.swift
@@ -6,26 +6,26 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ReviewModalView: View {
+    @Environment(\.dismiss) var dismiss
+    @Environment(\.modelContext) private var modelContext
+    
     var restaurant: Restaurant
     
-    @Environment(\.dismiss) var dismiss
-    
     @State private var selectedLabel: String = ""
+    
     @State var labelobjectsArray = [
         "밥약으로 가도 분위기 괜찮은 맛집",
         "각별한 선배와 밥약이나 교수님과 갈만한 맛집",
         "그냥 친구들이랑 가볍게 한 끼 먹기 좋은 식당",
         "술집에 가까운 식당 / 찐 술집",
-        "카페"]
+        "카페"
+    ]
         
     private let labelColors: [Color] = [
-        .red,
-        .blue,
-        .green,
-        .yellow,
-        .purple
+        .red, .blue, .green, .yellow, .purple
     ]
 
     var body: some View {
@@ -54,12 +54,10 @@ struct ReviewModalView: View {
                         selectedLabel = label
                     }
                 }
-                .border(Color.red)
             }
             
             Button{
-                
-                
+                updatesRestaurantLabel()
                 dismiss()
             } label: {
                 Text("리뷰 쓰기")
@@ -74,5 +72,26 @@ struct ReviewModalView: View {
         .background(Color(red: 0.1, green: 0.1, blue: 0.11))
     }
     
+    private func updatesRestaurantLabel() {
+        let labelMap: [String: Label] = [
+            "밥약으로 가도 분위기 괜찮은 맛집": .RED,
+            "각별한 선배와 밥약이나 교수님과 갈만한 맛집": .BLUE,
+            "그냥 친구들이랑 가볍게 한 끼 먹기 좋은 식당": .GREEN,
+            "술집에 가까운 식당 / 찐 술집": .YELLOW,
+            "카페": .PURPLE
+        ]
+        
+        if let newLabel = labelMap[selectedLabel] {
+            restaurant.label = newLabel
+            do {
+                try modelContext.save()
+                print("\(restaurant.label)로 업데이트됨")
+            } catch {
+                print("업데이트 실패")
+            }
+        } else {
+            print("매핑 실패")
+        }
+    }
 }
 
