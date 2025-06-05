325a25f2b86ab82e355e337d072adfc98ef4201d
HanHyeBeen <vvv5974033@gmail.com>
2025-06-05 16:28:28 +0900

    🔨 Feat | 필터링
    -필터링 모달 생성
    -필터링 모달 화면 그리기
    -필터링 모달 기능 구현
    -필터링 기능 구현
    -라벨 이미지 에셋 추가

--- F1T5/F1T5/Views/InfoView/InfoView.swift
+++ F1T5/F1T5/Views/InfoView/InfoView.swift
@@ -5,23 +5,186 @@
 //  Created by 진아현 on 5/30/25.
 //
 
-import Foundation
 import SwiftUI
 import SwiftData
 
 struct InformationView: View {
     @Query var restaurants: [Restaurant]
+    
     @Environment(\.modelContext) private var modelContext
     
+    @State private var showFilterModal = false
+    
+    @State private var selectedLabel: Label? = nil
+    @State private var selectedArea: String = ""
+    @State private var selectedFoods: Set<String> = []
+    
+    let labelMap: [String: Label] = [
+        "밥약으로 가도 분위기 괜찮은 맛집": .RED,
+        "각별한 선배, 교수님과 갈만한 맛집": .GREEN,
+        "친구들이랑 가볍게 먹기 좋은 식당": .BLUE,
+        "술집에 가까운 식당 / 찐 술집": .PURPLE,
+        "분위기 좋은 카페": .YELLOW
+    ]
+    
+    var filteredRestaurants: [Restaurant] {
+        restaurants.filter { restaurant in
+            let matchesLabel = selectedLabel == nil || restaurant.label == selectedLabel
+            let matchesArea = selectedArea.isEmpty || restaurant.area == selectedArea
+            let matchesFood = selectedFoods.isEmpty || selectedFoods.contains(restaurant.category.rawValue)
+            
+            return matchesLabel && matchesArea && matchesFood
+        }
+    }
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(restaurants) { restaurant in
-                    RestaurantList(for: restaurant)
+        NavigationStack {
+            VStack(alignment: .leading) {
+                // 타이틀
+                Text("리스트 보기")
+                    .foregroundColor(.white)
+                    .font(.title2.bold())
+                    .padding(.horizontal)
+                
+                // 필터 버튼 + 필터 태그
+                filterSection
+                
+                List {
+                    ForEach(filteredRestaurants) { restaurant in
+                        RestaurantList(for: restaurant)
+                    }
+                }
+                
+            }
+            .sheet(isPresented: $showFilterModal) {
+                FilterModalView(
+                    onApply: { labelString, area, foods in
+                        selectedLabel = labelMap[labelString]
+                        selectedArea = area
+                        selectedFoods = foods
+                    },
+                    selectedLabel: Binding(
+                        get: {
+                            if let label = selectedLabel {
+                                return key(for: label) ?? ""
+                            } else {
+                                return ""
+                            }
+                        },
+                        set: { selectedLabel = labelMap[$0] }
+                    ),
+
+                    selectedArea: $selectedArea,
+                    selectedFoods: $selectedFoods
+                )
+                .presentationDetents([.height(788)])
+                .presentationDragIndicator(.visible)
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden(true)
+            .background(Color.black.ignoresSafeArea())
+        }
+        
+    }
+    
+    var filterSection: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 8) {
+                Button {
+                    showFilterModal = true
+                } label: {
+                    Image(systemName: "slider.horizontal.3")
+                        .foregroundColor(.white)
+                }
+                .foregroundColor(.clear)
+                .frame(width: 28, height: 28)
+                .cornerRadius(8.37681)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8.37681)
+                        .inset(by: 0.5)
+                        .stroke(.white, lineWidth: 1)
+                )
+                .padding(.bottom, 24)
+                
+                if let selectedLabel = selectedLabel,
+                   let labelString = key(for: selectedLabel) {
+                    labelFilterTagView(label: labelString) {
+                        self.selectedLabel = nil
+                    }
+                }
+                
+                if !selectedArea.isEmpty {
+                    filterTagView(filter: selectedArea) { selectedArea = "" }
+                }
+                
+                foodFilterTags
+                
+            }
+            .padding(.horizontal, 10)
+            .padding(.vertical, 6)
+            .clipShape(Capsule())
+        }
+        .padding(.top, 4)
+    }
+    
+    var foodFilterTags: some View {
+        Group {
+            if !selectedFoods.isEmpty {
+                let first = selectedFoods.first ?? ""
+                let count = selectedFoods.count - 1
+                let label = count > 0 ? "\(first) 외 \(count)" : first
+                
+                filterTagView(filter: label) {
+                    selectedFoods.removeAll()
                 }
             }
         }
     }
+    
+    @ViewBuilder
+    private func labelFilterTagView(label: String, onRemove: @escaping () -> Void) -> some View {
+        HStack(spacing: 4) {
+            let imageName = labelMap[label]?.rawValue.lowercased() ?? "default"
+            
+
+            Image("\(imageName)_tag")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 24)
+            
+            Button(action: onRemove) {
+                Image(systemName: "xmark")
+                    .foregroundColor(.white)
+                    .font(.caption2)
+            }
+        }
+        .padding(.horizontal, 10)
+        .padding(.vertical, 6)
+        .background(Color.gray.opacity(0.4))
+        .clipShape(Capsule())
+    }
+    
+    @ViewBuilder
+    private func filterTagView(filter: String, onRemove: @escaping () -> Void) -> some View {
+        HStack(spacing: 4) {
+            Text(filter)
+                .foregroundColor(.white)
+            Button(action: onRemove) {
+                Image(systemName: "xmark")
+                    .foregroundColor(.white)
+                    .font(.caption2)
+            }
+        }
+        .padding(.horizontal, 10)
+        .padding(.vertical, 6)
+        .background(Color.gray.opacity(0.4))
+        .clipShape(Capsule())
+    }
+    
+    func key(for label: Label) -> String? {
+        return labelMap.first(where: { $0.value == label })?.key
+    }
+    
     @ViewBuilder
     private func RestaurantList(for restaurant: Restaurant) -> some View {
         ZStack {
@@ -114,7 +277,7 @@ struct InformationView: View {
             
             NavigationLink {
                 DetailView(restaurant: restaurant)
-            
+                
             } label: {
                 
             }.opacity(0.0)
